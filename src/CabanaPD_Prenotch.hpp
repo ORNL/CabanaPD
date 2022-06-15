880b616d977f92f3712daeec7d2426a203802979
Sam Reeve <6740307+streeve@users.noreply.github.com>
2022-06-15 14:18:15 -0400

    fixup: prenotch free functions

--- src/CabanaPD_Prenotch.hpp
+++ src/CabanaPD_Prenotch.hpp
@@ -63,6 +63,98 @@ Kokkos::Array<double, 3> sum( Kokkos::Array<double, 3> a,
     return c;
 }
 
+KOKKOS_INLINE_FUNCTION
+int line_plane_intersection( const Kokkos::Array<double, 3> p0,
+                             const Kokkos::Array<double, 3> n,
+                             const Kokkos::Array<double, 3> l0,
+                             const Kokkos::Array<double, 3> l,
+                             const double tol = 1e-10 )
+{
+    if ( abs( dot( l, n ) ) < tol )
+    {
+        if ( abs( dot( diff( p0, l0 ), n ) ) < tol )
+            return 1;
+        else
+            return 2;
+    }
+    return 3;
+}
+
+KOKKOS_INLINE_FUNCTION
+int bond_prenotch_intersection( const Kokkos::Array<double, 3> v1,
+                                const Kokkos::Array<double, 3> v2,
+                                const Kokkos::Array<double, 3> p0,
+                                const Kokkos::Array<double, 3> x_i,
+                                const Kokkos::Array<double, 3> x_j,
+                                const double tol = 1e-10 )
+{
+    // Define plane vectors cross product
+    auto cross_v1_v2 = cross( v1, v2 );
+    double norm_cross_v1_v2 = norm( cross_v1_v2 );
+
+    // Check if v1 and v2 are parallel
+    assert( abs( norm( cross( v1, v2 ) ) ) > tol );
+
+    // Define plane normal
+    auto n = scale( cross_v1_v2, 1.0 / norm_cross_v1_v2 );
+
+    // Define line
+    auto l0 = x_i;
+    auto l = diff( x_j, x_i );
+
+    // Check line-plane intersection
+    int case_flag = line_plane_intersection( p0, n, l0, l );
+    int keep_bond = 1;
+
+    // Case I: full intersection
+    if ( case_flag == 1 )
+    {
+        double norm2_cross_v1_v2 = norm_cross_v1_v2 * norm_cross_v1_v2;
+
+        double li1 = dot( cross( diff( x_i, p0 ), v2 ), cross_v1_v2 ) /
+                     norm2_cross_v1_v2;
+        double li2 = -dot( cross( diff( x_i, p0 ), v1 ), cross_v1_v2 ) /
+                     norm2_cross_v1_v2;
+
+        double lj1 = dot( cross( diff( x_j, p0 ), v2 ), cross_v1_v2 ) /
+                     norm2_cross_v1_v2;
+        double lj2 = -dot( cross( diff( x_j, p0 ), v1 ), cross_v1_v2 ) /
+                     norm2_cross_v1_v2;
+
+        if ( !( fmin( li1, lj1 ) > 1 + tol || fmax( li1, lj1 ) < -tol ||
+                fmin( li2, lj2 ) > 1 + tol || fmax( li2, lj2 ) < -tol ) )
+            keep_bond = 0;
+    }
+    // Case II: no intersection
+    // Case III: single point intersection
+    else if ( case_flag == 3 )
+    {
+        assert( abs( dot( l, n ) ) > tol );
+
+        // Check if intersection point belongs to the bond
+        auto d = dot( diff( p0, l0 ), n ) / dot( l, n );
+
+        // Check if intersection point belongs to the plane
+        if ( -tol < d && d < 1 + tol )
+        {
+            auto p = sum( l0, scale( l, d ) );
+
+            double norm2_cross_v1_v2 = norm_cross_v1_v2 * norm_cross_v1_v2;
+
+            double l1 = dot( cross( diff( p, p0 ), v2 ), cross_v1_v2 ) /
+                        norm2_cross_v1_v2;
+            double l2 = -dot( cross( diff( p, p0 ), v1 ), cross_v1_v2 ) /
+                        norm2_cross_v1_v2;
+
+            if ( -tol < fmin( l1, l2 ) && fmax( l1, l2 ) < 1 + tol )
+                // Intersection
+                keep_bond = 0;
+        }
+    }
+
+    return keep_bond;
+}
+
 template <std::size_t NumNotch>
 struct Prenotch
 {
@@ -99,7 +191,6 @@ struct Prenotch
                 std::size_t num_neighbors =
                     Cabana::NeighborList<Neighbors>::numNeighbor( neighbors,
                                                                   i );
-
                 Kokkos::Array<double, 3> xi;
                 Kokkos::Array<double, 3> xj;
                 for ( std::size_t n = 0; n < num_neighbors; n++ )
@@ -120,98 +211,6 @@ struct Prenotch
             };
             Kokkos::parallel_for( policy, notch_functor, "CabanaPD::Prenotch" );
         }
-    }
-
-    KOKKOS_INLINE_FUNCTION
-    int line_plane_intersection( Kokkos::Array<double, 3> p0,
-                                 Kokkos::Array<double, 3> n,
-                                 Kokkos::Array<double, 3> l0,
-                                 Kokkos::Array<double, 3> l,
-                                 double tol = 1e-10 )
-    {
-        if ( abs( dot( l, n ) ) < tol )
-        {
-            if ( abs( dot( diff( p0, l0 ), n ) ) < tol )
-                return 1;
-            else
-                return 2;
-        }
-        return 3;
-    }
-
-    KOKKOS_INLINE_FUNCTION
-    int bond_prenotch_intersection( Kokkos::Array<double, 3> v1,
-                                    Kokkos::Array<double, 3> v2,
-                                    Kokkos::Array<double, 3> p0,
-                                    Kokkos::Array<double, 3> x_i,
-                                    Kokkos::Array<double, 3> x_j,
-                                    double tol = 1e-10 )
-    {
-        // Define plane vectors cross product
-        auto cross_v1_v2 = cross( v1, v2 );
-        double norm_cross_v1_v2 = norm( cross_v1_v2 );
-
-        // Check if v1 and v2 are parallel
-        assert( abs( norm( cross( v1, v2 ) ) ) > tol );
-
-        // Define plane normal
-        auto n = scale( cross_v1_v2, 1.0 / norm_cross_v1_v2 );
-
-        // Define line
-        auto l0 = x_i;
-        auto l = diff( x_j, x_i );
-
-        // Check line-plane intersection
-        int case_flag = line_plane_intersection( p0, n, l0, l );
-        int keep_bond = 1;
-
-        // Case I: full intersection
-        if ( case_flag == 1 )
-        {
-            double norm2_cross_v1_v2 = norm_cross_v1_v2 * norm_cross_v1_v2;
-
-            double li1 = dot( cross( diff( x_i, p0 ), v2 ), cross_v1_v2 ) /
-                         norm2_cross_v1_v2;
-            double li2 = -dot( cross( diff( x_i, p0 ), v1 ), cross_v1_v2 ) /
-                         norm2_cross_v1_v2;
-
-            double lj1 = dot( cross( diff( x_j, p0 ), v2 ), cross_v1_v2 ) /
-                         norm2_cross_v1_v2;
-            double lj2 = -dot( cross( diff( x_j, p0 ), v1 ), cross_v1_v2 ) /
-                         norm2_cross_v1_v2;
-
-            if ( !( fmin( li1, lj1 ) > 1 + tol || fmax( li1, lj1 ) < -tol ||
-                    fmin( li2, lj2 ) > 1 + tol || fmax( li2, lj2 ) < -tol ) )
-                keep_bond = 0;
-        }
-        // Case II: no intersection
-        // Case III: single point intersection
-        else if ( case_flag == 3 )
-        {
-            assert( abs( dot( l, n ) ) > tol );
-
-            // Check if intersection point belongs to the bond
-            auto d = dot( diff( p0, l0 ), n ) / dot( l, n );
-
-            // Check if intersection point belongs to the plane
-            if ( -tol < d && d < 1 + tol )
-            {
-                auto p = sum( l0, scale( l, d ) );
-
-                double norm2_cross_v1_v2 = norm_cross_v1_v2 * norm_cross_v1_v2;
-
-                double l1 = dot( cross( diff( p, p0 ), v2 ), cross_v1_v2 ) /
-                            norm2_cross_v1_v2;
-                double l2 = -dot( cross( diff( p, p0 ), v1 ), cross_v1_v2 ) /
-                            norm2_cross_v1_v2;
-
-                if ( -tol < fmin( l1, l2 ) && fmax( l1, l2 ) < 1 + tol )
-                    // Intersection
-                    keep_bond = 0;
-            }
-        }
-
-        return keep_bond;
     }
 };
 
