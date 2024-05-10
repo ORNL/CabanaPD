a01dea33998e7b873685b675393a3953d09d0b78
Sam Reeve <6740307+streeve@users.noreply.github.com>
2024-05-10 16:00:04 -0400

    Separate Kokkos+MPI init from examples

--- examples/crack_branching.cpp
+++ examples/crack_branching.cpp
@@ -18,134 +18,137 @@
 
 #include <CabanaPD.hpp>
 
+// Simulate crack branching from an pre-crack.
+void crackBranchingExample( const std::string filename )
+{
+    // ====================================================
+    //                  Use default Kokkos spaces
+    // ====================================================
+    using exec_space = Kokkos::DefaultExecutionSpace;
+    using memory_space = typename exec_space::memory_space;
+
+    // ====================================================
+    //                   Read inputs
+    // ====================================================
+    CabanaPD::Inputs inputs( filename );
+
+    // ====================================================
+    //                Material parameters
+    // ====================================================
+    double rho0 = inputs["density"];
+    double E = inputs["elastic_modulus"];
+    double nu = 0.25;
+    double K = E / ( 3 * ( 1 - 2 * nu ) );
+    double G0 = inputs["fracture_energy"];
+    double delta = inputs["horizon"];
+    delta += 1e-10;
+
+    // ====================================================
+    //                  Discretization
+    // ====================================================
+    // FIXME: set halo width based on delta
+    std::array<double, 3> low_corner = inputs["low_corner"];
+    std::array<double, 3> high_corner = inputs["high_corner"];
+    std::array<int, 3> num_cells = inputs["num_cells"];
+    int m = std::floor( delta /
+                        ( ( high_corner[0] - low_corner[0] ) / num_cells[0] ) );
+    int halo_width = m + 1; // Just to be safe.
+
+    // ====================================================
+    //                    Pre-notch
+    // ====================================================
+    double height = inputs["system_size"][0];
+    double thickness = inputs["system_size"][2];
+    double L_prenotch = height / 2.0;
+    double y_prenotch1 = 0.0;
+    Kokkos::Array<double, 3> p01 = { low_corner[0], y_prenotch1,
+                                     low_corner[2] };
+    Kokkos::Array<double, 3> v1 = { L_prenotch, 0, 0 };
+    Kokkos::Array<double, 3> v2 = { 0, 0, thickness };
+    Kokkos::Array<Kokkos::Array<double, 3>, 1> notch_positions = { p01 };
+    CabanaPD::Prenotch<1> prenotch( v1, v2, notch_positions );
+
+    // ====================================================
+    //                    Force model
+    // ====================================================
+    using model_type = CabanaPD::ForceModel<CabanaPD::PMB, CabanaPD::Fracture>;
+    model_type force_model( delta, K, G0 );
+
+    // ====================================================
+    //                 Particle generation
+    // ====================================================
+    // Does not set displacements, velocities, etc.
+    auto particles = std::make_shared<
+        CabanaPD::Particles<memory_space, typename model_type::base_model>>(
+        exec_space(), low_corner, high_corner, num_cells, halo_width );
+
+    // ====================================================
+    //                Boundary conditions
+    // ====================================================
+    double sigma0 = inputs["traction"];
+    double dy = particles->dx[1];
+    double b0 = sigma0 / dy;
+
+    CabanaPD::RegionBoundary plane1( low_corner[0], high_corner[0],
+                                     low_corner[1] - dy, low_corner[1] + dy,
+                                     low_corner[2], high_corner[2] );
+    CabanaPD::RegionBoundary plane2( low_corner[0], high_corner[0],
+                                     high_corner[1] - dy, high_corner[1] + dy,
+                                     low_corner[2], high_corner[2] );
+    std::vector<CabanaPD::RegionBoundary> planes = { plane1, plane2 };
+    auto particles_f = particles->getForce();
+    auto particles_x = particles->getReferencePosition();
+    // Create a symmetric force BC in the y-direction.
+    auto bc_op = KOKKOS_LAMBDA( const int pid )
+    {
+        // Get a modifiable copy of force.
+        auto p_f = particles_f.getParticleView( pid );
+        // Get a copy of the position.
+        auto p_x = particles_x.getParticle( pid );
+        auto ypos = Cabana::get( p_x, CabanaPD::Field::ReferencePosition(), 1 );
+        auto sign = std::abs( ypos ) / ypos;
+        Cabana::get( p_f, CabanaPD::Field::Force(), 1 ) += b0 * sign;
+    };
+    auto bc =
+        createBoundaryCondition( bc_op, exec_space{}, *particles, planes );
+
+    // ====================================================
+    //            Custom particle initialization
+    // ====================================================
+    auto rho = particles->sliceDensity();
+    auto x = particles->sliceReferencePosition();
+    auto v = particles->sliceVelocity();
+    auto f = particles->sliceForce();
+    auto nofail = particles->sliceNoFail();
+
+    auto init_functor = KOKKOS_LAMBDA( const int pid )
+    {
+        // Density
+        rho( pid ) = rho0;
+        // No-fail zone
+        if ( x( pid, 1 ) <= plane1.low_y + delta + 1e-10 ||
+             x( pid, 1 ) >= plane2.high_y - delta - 1e-10 )
+            nofail( pid ) = 1;
+    };
+    particles->updateParticles( exec_space{}, init_functor );
+
+    // ====================================================
+    //                   Simulation run
+    // ====================================================
+    auto cabana_pd = CabanaPD::createSolverFracture<memory_space>(
+        inputs, particles, force_model, bc, prenotch );
+    cabana_pd->init_force();
+    cabana_pd->run();
+}
+
+// Initialize MPI+Kokkos.
 int main( int argc, char* argv[] )
 {
     MPI_Init( &argc, &argv );
+    Kokkos::initialize( argc, argv );
 
-    {
-        // ====================================================
-        //                  Setup Kokkos
-        // ====================================================
-        Kokkos::ScopeGuard scope_guard( argc, argv );
+    crackBranchingExample( argv[1] );
 
-        using exec_space = Kokkos::DefaultExecutionSpace;
-        using memory_space = typename exec_space::memory_space;
-
-        // ====================================================
-        //                   Read inputs
-        // ====================================================
-        CabanaPD::Inputs inputs( argv[1] );
-
-        // ====================================================
-        //                Material parameters
-        // ====================================================
-        double rho0 = inputs["density"];
-        double E = inputs["elastic_modulus"];
-        double nu = 0.25;
-        double K = E / ( 3 * ( 1 - 2 * nu ) );
-        double G0 = inputs["fracture_energy"];
-        double delta = inputs["horizon"];
-        delta += 1e-10;
-
-        // ====================================================
-        //                  Discretization
-        // ====================================================
-        // FIXME: set halo width based on delta
-        std::array<double, 3> low_corner = inputs["low_corner"];
-        std::array<double, 3> high_corner = inputs["high_corner"];
-        std::array<int, 3> num_cells = inputs["num_cells"];
-        int m = std::floor(
-            delta / ( ( high_corner[0] - low_corner[0] ) / num_cells[0] ) );
-        int halo_width = m + 1; // Just to be safe.
-
-        // ====================================================
-        //                    Pre-notch
-        // ====================================================
-        double height = inputs["system_size"][0];
-        double thickness = inputs["system_size"][2];
-        double L_prenotch = height / 2.0;
-        double y_prenotch1 = 0.0;
-        Kokkos::Array<double, 3> p01 = { low_corner[0], y_prenotch1,
-                                         low_corner[2] };
-        Kokkos::Array<double, 3> v1 = { L_prenotch, 0, 0 };
-        Kokkos::Array<double, 3> v2 = { 0, 0, thickness };
-        Kokkos::Array<Kokkos::Array<double, 3>, 1> notch_positions = { p01 };
-        CabanaPD::Prenotch<1> prenotch( v1, v2, notch_positions );
-
-        // ====================================================
-        //                    Force model
-        // ====================================================
-        using model_type =
-            CabanaPD::ForceModel<CabanaPD::PMB, CabanaPD::Fracture>;
-        model_type force_model( delta, K, G0 );
-
-        // ====================================================
-        //                 Particle generation
-        // ====================================================
-        // Does not set displacements, velocities, etc.
-        auto particles = std::make_shared<
-            CabanaPD::Particles<memory_space, typename model_type::base_model>>(
-            exec_space(), low_corner, high_corner, num_cells, halo_width );
-
-        // ====================================================
-        //                Boundary conditions
-        // ====================================================
-        double sigma0 = inputs["traction"];
-        double dy = particles->dx[1];
-        double b0 = sigma0 / dy;
-
-        CabanaPD::RegionBoundary plane1( low_corner[0], high_corner[0],
-                                         low_corner[1] - dy, low_corner[1] + dy,
-                                         low_corner[2], high_corner[2] );
-        CabanaPD::RegionBoundary plane2(
-            low_corner[0], high_corner[0], high_corner[1] - dy,
-            high_corner[1] + dy, low_corner[2], high_corner[2] );
-        std::vector<CabanaPD::RegionBoundary> planes = { plane1, plane2 };
-        auto particles_f = particles->getForce();
-        auto particles_x = particles->getReferencePosition();
-        // Create a symmetric force BC in the y-direction.
-        auto bc_op = KOKKOS_LAMBDA( const int pid )
-        {
-            // Get a modifiable copy of force.
-            auto p_f = particles_f.getParticleView( pid );
-            // Get a copy of the position.
-            auto p_x = particles_x.getParticle( pid );
-            auto ypos =
-                Cabana::get( p_x, CabanaPD::Field::ReferencePosition(), 1 );
-            auto sign = std::abs( ypos ) / ypos;
-            Cabana::get( p_f, CabanaPD::Field::Force(), 1 ) += b0 * sign;
-        };
-        auto bc =
-            createBoundaryCondition( bc_op, exec_space{}, *particles, planes );
-
-        // ====================================================
-        //            Custom particle initialization
-        // ====================================================
-        auto rho = particles->sliceDensity();
-        auto x = particles->sliceReferencePosition();
-        auto v = particles->sliceVelocity();
-        auto f = particles->sliceForce();
-        auto nofail = particles->sliceNoFail();
-
-        auto init_functor = KOKKOS_LAMBDA( const int pid )
-        {
-            // Density
-            rho( pid ) = rho0;
-            // No-fail zone
-            if ( x( pid, 1 ) <= plane1.low_y + delta + 1e-10 ||
-                 x( pid, 1 ) >= plane2.high_y - delta - 1e-10 )
-                nofail( pid ) = 1;
-        };
-        particles->updateParticles( exec_space{}, init_functor );
-
-        // ====================================================
-        //                   Simulation run
-        // ====================================================
-        auto cabana_pd = CabanaPD::createSolverFracture<memory_space>(
-            inputs, particles, force_model, bc, prenotch );
-        cabana_pd->init_force();
-        cabana_pd->run();
-    }
-
+    Kokkos::finalize();
     MPI_Finalize();
 }
