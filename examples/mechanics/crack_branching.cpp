d15f0fbdf7057ad3366e2f42603ae971c0e677c8
psc <psc@pscstudio.ornl.gov>
2024-06-13 17:34:12 -0400

    Added solver heading and separate it from run heading for consistency

--- examples/mechanics/crack_branching.cpp
+++ examples/mechanics/crack_branching.cpp
@@ -134,10 +134,14 @@ void crackBranchingExample( const std::string filename )
     particles->updateParticles( exec_space{}, init_functor );
 
     // ====================================================
-    //                   Simulation run
+    //                   Create solver
     // ====================================================
     auto cabana_pd = CabanaPD::createSolverFracture<memory_space>(
         inputs, particles, force_model, prenotch );
+
+    // ====================================================
+    //                   Simulation run
+    // ====================================================
     cabana_pd->init();
     cabana_pd->run( bc );
 }
