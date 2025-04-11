2742c0531660dd84d13c0f3d7b4548566be33cb5
pabloseleson <selesonpd@ornl.gov>
2025-04-11 19:20:53 -0400

    Updated HIP cylinder example; new model form

--- examples/thermomechanics/hip_cylinder.cpp
+++ examples/thermomechanics/hip_cylinder.cpp
@@ -36,18 +36,20 @@ void HIPCylinderExample( const std::string filename )
     //                Material parameters
     // ====================================================
     double rho0 = inputs["density"];
-    double K = inputs["bulk_modulus"];
-    // double G = inputs["shear_modulus"]; // Only for LPS.
-    double sc = inputs["critical_stretch"];
+    double E = inputs["elastic_modulus"];
+    double nu = 0.25; // Use bond-based model
+    double K = E / ( 3 * ( 1 - 2 * nu ) );
+    double G0 = inputs["fracture_energy"];
+    double sigma_y = inputs["yield_stress"];
     double delta = inputs["horizon"];
     delta += 1e-10;
     // For PMB or LPS with influence_type == 1
-    double G0 = 9 * K * delta * ( sc * sc ) / 5;
+    // double G0 = 9 * K * delta * ( sc * sc ) / 5;
     // For LPS with influence_type == 0 (default)
     // double G0 = 15 * K * delta * ( sc * sc ) / 8;
-    double sigma_y = 10.0;
     double alpha = inputs["thermal_expansion_coeff"];
     double temp0 = inputs["reference_temperature"];
+    double sigma0 = inputs["traction"];
 
     // ====================================================
     //                  Discretization
@@ -60,7 +62,7 @@ void HIPCylinderExample( const std::string filename )
     int halo_width = m + 1; // Just to be safe.
 
     // ====================================================
-    //                    Force model
+    //                Force model type
     // ====================================================
     using model_type = CabanaPD::PMB;
     using mechanics_type = CabanaPD::ElasticPerfectlyPlastic;
@@ -94,39 +96,116 @@ void HIPCylinderExample( const std::string filename )
 
     auto rho = particles.sliceDensity();
     auto x = particles.sliceReferencePosition();
-    auto v = particles.sliceVelocity();
-    auto f = particles.sliceForce();
-
-    double vrmax = inputs["max_radial_velocity"];
-    double vrmin = inputs["min_radial_velocity"];
-    double vzmax = inputs["max_vertical_velocity"];
-    double zmin = z_center - 0.5 * H;
+    double W = inputs["wall_thickness"];
 
     auto init_functor = KOKKOS_LAMBDA( const int pid )
     {
-        // Density
-        rho( pid ) = rho0;
-
-        // Velocity
-        double zfactor = ( ( x( pid, 2 ) - zmin ) / ( 0.5 * H ) ) - 1;
-        double vr = vrmax - vrmin * zfactor * zfactor;
-        v( pid, 0 ) =
-            vr * Kokkos::cos( Kokkos::atan2( x( pid, 1 ), x( pid, 0 ) ) );
-        v( pid, 1 ) =
-            vr * Kokkos::sin( Kokkos::atan2( x( pid, 1 ), x( pid, 0 ) ) );
-        v( pid, 2 ) = vzmax * zfactor;
+        double rsq = ( x( pid, 0 ) - x_center ) * ( x( pid, 0 ) - x_center ) +
+                     ( x( pid, 1 ) - y_center ) * ( x( pid, 1 ) - y_center );
+        if ( rsq > ( Rin + W ) * ( Rin + W ) &&
+             rsq > ( Rout - W ) * ( Rout - W ) &&
+             x( pid, 2 ) > low_corner[2] + W )
+        { // Powder density
+            rho( pid ) = 0.7 * rho0;
+        }
+        else
+        { // Container density
+            rho( pid ) = rho0;
+        };
     };
     particles.updateParticles( exec_space{}, init_functor );
 
+    // ====================================================
+    //                Boundary conditions planes
+    // ====================================================
+    CabanaPD::RegionBoundary<CabanaPD::RectangularPrism> plane(
+        low_corner[0], high_corner[0], low_corner[1], high_corner[1],
+        low_corner[2], high_corner[2] );
+
+    // ====================================================
+    //                    Force model
+    // ====================================================
     rho = particles.sliceDensity();
     auto temp = particles.sliceTemperature();
     CabanaPD::ForceDensityModel force_model( model_type{}, mechanics_type{},
                                              rho, delta, K, G0, sigma_y, rho0,
                                              temp, alpha, temp0 );
 
+    // ====================================================
+    //                   Create solver
+    // ====================================================
     CabanaPD::Solver solver( inputs, particles, force_model );
-    solver.init();
-    solver.run();
+
+    // ====================================================
+    //                Boundary conditions
+    // ====================================================
+
+    // Create BC last to ensure ghost particles are included.
+    double dx = particles.dx[0];
+    // double dy = particles.dx[1];
+    double dz = particles.dx[2];
+    // double sigma0 = inputs["traction"];
+    double b0 = sigma0 / dx;
+    auto f = particles.sliceForce();
+    x = particles.sliceReferencePosition();
+    // Create an isostatic pressure BC.
+    auto bc_op = KOKKOS_LAMBDA( const int pid, const double )
+    {
+        double rsq = ( x( pid, 0 ) - x_center ) * ( x( pid, 0 ) - x_center ) +
+                     ( x( pid, 1 ) - y_center ) * ( x( pid, 1 ) - y_center );
+        double theta =
+            Kokkos::atan2( x( pid, 1 ) - y_center, x( pid, 0 ) - x_center );
+
+        // BC on outer boundary
+        if ( rsq > ( Rout - dx ) * ( Rout - dx ) )
+        {
+            f( pid, 0 ) += -b0 * Kokkos::cos( theta );
+            f( pid, 1 ) += -b0 * Kokkos::sin( theta );
+        }
+        // BC on inner boundary
+        else if ( rsq < ( Rin + dx ) * ( Rin + dx ) )
+        {
+            f( pid, 0 ) += b0 * Kokkos::cos( theta );
+            f( pid, 1 ) += b0 * Kokkos::sin( theta );
+        };
+
+        // BC on top boundary
+        if ( x( pid, 2 ) > z_center + 0.5 * H - dz )
+        {
+            f( pid, 2 ) += -b0;
+        }
+        // BC on bottom boundary
+        else if ( x( pid, 2 ) < z_center - 0.5 * H + dz )
+        {
+            f( pid, 2 ) += b0;
+        };
+    };
+    auto bc =
+        createBoundaryCondition( bc_op, exec_space{}, particles, true, plane );
+
+    // ====================================================
+    //                   Imposed field
+    // ====================================================
+    /*
+    x = particles.sliceReferencePosition();
+    auto temp = particles.sliceTemperature();
+    const double low_corner_y = low_corner[1];
+    // This is purposely delayed until after solver init so that ghosted
+    // particles are correctly taken into account for lambda capture here.
+    auto temp_func = KOKKOS_LAMBDA( const int pid, const double t )
+    {
+        temp( pid ) = temp0 + 5000.0 * ( x( pid, 1 ) - low_corner_y ) * t;
+    };
+    CabanaPD::BodyTerm body_term( temp_func, particles.size(), false );
+    */
+
+    // ====================================================
+    //                   Simulation run
+    // ====================================================
+    solver.init( bc );
+    solver.run( bc );
+    // solver.init( body_term );
+    // solver.run( body_term );
 }
 
 // Initialize MPI+Kokkos.
