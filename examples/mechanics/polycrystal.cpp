f1e3de834c4854fe3f4db44f522b58617ef7d85e
Jacob Hauck <jphqkc@umsystem.edu>
2026-05-19 13:40:36 -0500

    Formatting

--- examples/mechanics/polycrystal.cpp
+++ examples/mechanics/polycrystal.cpp
@@ -232,7 +232,7 @@ void getPolycrystalGrains(
 {
     // Initialize RNG (for now with FIXED seed)
     std::minstd_rand baseRng;
-    baseRng.seed(12345);
+    baseRng.seed( 12345 );
     std::seed_seq randomSeed{ baseRng(), baseRng(), baseRng(), baseRng(),
                               baseRng(), baseRng(), baseRng(), baseRng() };
     std::mt19937 gen( randomSeed );
@@ -277,16 +277,12 @@ void polycrystalExample( const std::string filename )
     // ====================================================
     //                  Discretization
     // ====================================================
-    std::array<double, 3> low_corner = {
-        inputs["low_corner"][0],
-        inputs["low_corner"][1],
-        inputs["low_corner"][2]
-    };
-    std::array<double, 3> high_corner = {
-        inputs["high_corner"][0],
-        inputs["high_corner"][1],
-        inputs["high_corner"][2]
-    };
+    std::array<double, 3> low_corner = { inputs["low_corner"][0],
+                                         inputs["low_corner"][1],
+                                         inputs["low_corner"][2] };
+    std::array<double, 3> high_corner = { inputs["high_corner"][0],
+                                          inputs["high_corner"][1],
+                                          inputs["high_corner"][2] };
 
     // ====================================================
     //                Material parameters
@@ -329,20 +325,6 @@ void polycrystalExample( const std::string filename )
                         grainPosStd[i][1] + low_corner[1],
                         grainPosStd[i][2] + low_corner[2] };
     }
-
-    // No pre-notch
-    // // ====================================================
-    // //                    Pre-notch
-    // // ====================================================
-    // double height = inputs["system_size"][0];
-    // double thickness = inputs["system_size"][2];
-    // double L_prenotch = height / 2.0;
-    // double y_prenotch = 0.5 * ( low_corner[1] + high_corner[1] );
-    // Kokkos::Array<double, 3> p01 = { low_corner[0], y_prenotch, low_corner[2] };
-    // Kokkos::Array<double, 3> v1 = { L_prenotch, 0, 0 };
-    // Kokkos::Array<double, 3> v2 = { 0, 0, thickness };
-    // Kokkos::Array<Kokkos::Array<double, 3>, 1> notch_positions = { p01 };
-    // CabanaPD::Prenotch<1> prenotch( v1, v2, notch_positions );
 
     // ====================================================
     //                   Force models
@@ -439,24 +421,6 @@ void polycrystalExample( const std::string filename )
     };
     auto bc = createBoundaryCondition( bc_op, exec_space{}, solver.particles,
                                        true, plane1, plane2 );
-
-    // // ====================================================
-    // //                      Outputs
-    // // ====================================================
-    // // Output maximum y-extent of the crack.
-    // CabanaPD::Region<CabanaPD::RectangularPrism> box( low_corner, high_corner );
-    // auto d = solver.particles.sliceDamage();
-    // auto crack_y_func = KOKKOS_LAMBDA( const int p )
-    // {
-    //     // Use a threshold of damage to only output damaged particles.
-    //     if ( d( p ) > 0.3 )
-    //         return x( p, 1 );
-    //     else
-    //         return 0.0;
-    // };
-    // auto output_yl = CabanaPD::createOutputTimeSeries<Kokkos::Max<double>>(
-    //     "output_polycrystal_crack_y.txt", inputs, exec_space{},
-    //     solver.particles, crack_y_func, box );
 
     // ====================================================
     //                   Simulation run
